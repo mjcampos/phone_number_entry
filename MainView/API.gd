4a15886e662ff84f28fd1aaf4395c65e252de812
Michael <camposmj89@gmail.com>
2024-03-02 13:49:00 -0800

    Removed numbers API call due to itch.io blocking access to it

--- MainView/API.gd
+++ MainView/API.gd
@@ -1,39 +1,30 @@
 extends Node
 
-@onready var http_request: HTTPRequest = %HTTPRequest
 @onready var main_container: MarginContainer = %MainContainer
 @onready var save_button: Button = %SaveButton
 @onready var error_message: Label = %ErrorMessage
 
-const URL = "http://numbersapi.com/"
-const suffix = "/trivia?fragment"
-
-var phone_number_array: Array
+const repo = { 
+	"0": "the coldest possible temperature old the Kelvin scale", 
+	"1": "the number of dimensions of a line", 
+	"2": "the lowest channel of television in the United States, Canada, Argentina and Mexico on which television signals are broadcast", 
+	"3": "the number of witches in William Shakespeare's Macbeth", 
+	"4": "the number of legs most furniture has", 
+	"5": "the number of completed, numbered piano concertos of Ludwig van Beethoven, Sergei Prokofiev, and Camille Saint-Saëns", 
+	"6": "the number of points on a Star of David", 
+	"7": "the number of types of viruses according to the Baltimore classification", 
+	"8": "the number of furlongs in a mile", 
+	"9": "the number of circles of Hell in Dante's Divine Comedy" 
+}
 
 func make_api_call(phone_number: int) -> void:
-	var stringed_phone_number: String = integer_with_commas(phone_number)
-	var modified_url = URL + stringed_phone_number + suffix
+	var phone_number_array = integer_to_digit_array(phone_number)
+	var random_number_facts = {}
 	
-	phone_number_array = integer_to_digit_array(phone_number)
-	save_button.disabled = true
-	error_message.visible = false
+	for number_string in phone_number_array:
+		random_number_facts[number_string] = repo[number_string]
 	
-	$HTTPRequest.request(modified_url)
-
-func integer_with_commas(number: int) -> String:
-	var number_str = str(number)  # Convert integer to string
-	var result = ""  # Initialize result string
-	
-	# Iterate over each character in the string
-	for i in range(number_str.length()):
-		# Append character to result string
-		result += number_str[i]
-		
-		# Add comma if it's not the last character
-		if i < number_str.length() - 1:
-			result += ","
-	
-	return result
+	main_container.show_facts(random_number_facts, phone_number_array)
 
 func integer_to_digit_array(number: int) -> Array:
 	var number_str = str(number)  # Convert integer to string
@@ -45,15 +36,3 @@ func integer_to_digit_array(number: int) -> Array:
 		digit_array.append(number_str[i])
 
 	return digit_array
-
-func _on_http_request_request_completed(result: int, response_code: int, headers: PackedStringArray, body: PackedByteArray) -> void:
-	save_button.disabled = false
-	
-	if response_code == 200:
-		# Parse the returned body
-		var random_number_facts = JSON.parse_string(body.get_string_from_utf8())
-		
-		main_container.show_facts(random_number_facts, phone_number_array)
-	else:
-		print("We have a problem")
-		error_message.visible = true
